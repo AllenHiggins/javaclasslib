337d95d45ef59185b16a4f8938553ae8203973fe
AllenHiggins <allenghiggins@gmail.com>
2018-07-08 13:16:17 +0100

    cleaned up main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,10 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    //    int[] list = {1,2,3,4,5};
-    //    MaxMin maxMin = new MaxMin();
-    //    String result = maxMin.findMaxMin(list);
-    //    System.out.println(result);
 
-        int[] list = {7};
-        List<Integer> result;
-        NotDiv notDiv = new NotDiv();
-        result = notDiv.isNotDiv(list.length,list,0);
-        System.out.println(result);
+        // Write Your Code Here
+
     }
 }
